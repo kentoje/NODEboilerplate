e4aee6a07590039c23f58ba69f7820c9eff887f5
kentoje <k.monthubert@gmail.com>
2019-05-15 15:45:37 +0200

    add crud in api router

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,22 +1,42 @@
 /*
 Configurer le module de route
 */
-const express = require('express');
-const router = express.Router();
+  const express = require('express')
+  const router = express.Router()
 //
 
 /*
-Définition des routes
+Définition du CRUD
 */
-// Accueil
-router.get('/', (req, res) => {
-    res.json({ msg: 'Hello API' })
-});
+
+  // Create item: POST
+  router.post('/article', (req, res) => {
+      res.json({ msg: 'Create Article' })
+  })
+  // Read all item : GET
+  router.get('/article', (req, res) => {
+      res.json({ msg: 'Read all Article' })
+  })
+
+  // Read one item : GET
+  router.get('/article/:id', (req, res) => {
+      res.json({ msg: 'Read one Article' })
+  })
+
+  // Update : PUT
+  router.put('/article/:id', (req, res) => {
+      res.json({ msg: 'Update one Article' })
+  })
+
+  // Delete one item : DELETE
+  router.delete('/article/:id', (req, res) => {
+      res.json({ msg: 'Delete one Article' })
+  })
 //
 
 
 /*
 Exporter le module de route
 */
-module.exports = router;
+  module.exports = router
 //
