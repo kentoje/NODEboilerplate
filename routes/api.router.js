a0275ac61e8f0f54edad3d41070ff1c90927bee9
kentoje <k.monthubert@gmail.com>
2019-05-15 16:56:10 +0200

    add create item in api

--- routes/api.router.js
+++ routes/api.router.js
@@ -27,15 +27,32 @@ Définition du CRUD
 
   // Create item: POST
   router.post('/article', (req, res) => {
-    
+
     /* 
       Pour créer un article il faut une valeur pour :
       - title
       - content
     */
 
+      if ( req.body && req.body.title.length > 0 && req.body.content.length > 0 ) {
+        // Définir l'item
+        const newItem = { title : req.body.title, content : req.body.content }
 
-    res.json({ msg: 'Create Article' })
+        // Enregistre l'item
+        connexion.query( `INSERT INTO post SET ?`, newItem, ( err, result, fields ) => {
+          if( err ) {
+            res.json({ msg: 'Connection failed', data: err })    
+          } else {
+            res.json({ msg: 'Create Article', data: result })
+          }
+        } )
+
+        res.json({ msg: 'Create Article', data: req.body })
+      } else {
+        res.json({ msg: 'No data', data: null })
+      }
+    //  
+
   })
   // Read all item : GET
   router.get('/article', (req, res) => {
